66fe83883384b478812d2c450778eddfd405f148
baly <kevin@baldev.it>
2022-08-26 14:43:05 +0200

    Project Done

---
+++ src/components/AppCharacters.vue
@@ -0,0 +1,43 @@
+<template>
+    <main class="container py-5">
+        <div class="row d-flex flex-wrap">
+            <AppSingleCharacters class="col-4" v-for="character in characters" :key="character.id" :character="character"/>
+        </div>
+    </main>
+</template>
+
+<script>
+import axios from 'axios'
+import AppSingleCharacters from './AppSingleCharacters.vue'
+
+export default{
+    name: 'AppCharacters',
+    components:{
+    AppSingleCharacters,
+    },
+    data(){
+        return{
+            characters: [],
+            endpoint: 'https://api.sampleapis.com/rickandmorty/characters'
+        }
+    },
+    created(){
+        this.getApiData()
+    },
+    methods:{
+        getApiData: function(){
+            axios.get(this.endpoint)
+            .then( response => {
+                this.characters = response.data
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/AppSingleCharacters.vue
@@ -0,0 +1,34 @@
+<template>
+    
+    <div class="card">
+        <div class="card-header">
+            <img :src="character.image" alt="">
+        </div>
+        <div class="card-body">
+            <h3>{{character.name}}</h3>
+            <hr>
+            <p>{{character.origin}}</p>
+            <p>{{character.species}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'AppSingleCharacters',
+    props:{
+        character: Object
+    }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+.card-header{
+    img{
+        width: 100%;
+    }
+}
+</style>
